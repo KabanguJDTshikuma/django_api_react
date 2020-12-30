50e65a9e294cb4637e4d65499ebbf62407e20f01
Nava <nava@Users-MacBook-Pro.local>
2020-12-31 01:15:11 +0200

    added a blog test

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from blog.models import Post, User
+from .serializers import PostSerializer
 
-class PostList:
-    pass
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.postobjects.all()
+    serializer_class = PostSerializer
 
-class PostDetail:
-    pass
+class PostDetail(generics.RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
